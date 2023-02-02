765218651ebde7b016354eb8cf7a734f469904c2
mgillis33 <gillism@emmanuel.edu>
2023-02-01 22:31:44 -0500

    Changed theme components, title sizes, colors, etc.

--- lebron.R
+++ lebron.R
@@ -7,8 +7,9 @@ lebron <- read.csv("/Users/mgillis/Desktop/Projects/nba-scoring/data/Lebron.csv"
 
 # build plot
 points <- ggplot(lebron, aes(x = Game, y = CUMPTS)) +
-  geom_line(linewidth = 1) +
-  geom_hline(yintercept = 38387, linewidth = 1) +
+  geom_line(linewidth = 1, color = "#EAEAEA") +
+  geom_hline(yintercept = 38387, linewidth = 1, color = "#EAEAEA") +
+  geom_point(size = 2, color = "#EAEAEA") + 
   labs(
     title = "Lebron James: Journey to Career Points Record",
     subtitle = "An animation of Lebron's total career points, as he breaks Kareem Abdul-Jabbar's record for most career points.",
@@ -16,20 +17,26 @@ points <- ggplot(lebron, aes(x = Game, y = CUMPTS)) +
     y = "Points (Total)"
     ) +
   theme(
-    text = element_text(family = "Rockwell"),
-    plot.title = element_text(face = "bold"),
-    axis.title = element_text(face = "bold")
+    text = element_text(family = "Rockwell", color = "#EAEAEA"),
+    plot.title = element_text(face = "bold", size = 33),
+    plot.subtitle = element_text(size = 15),
+    axis.title = element_text(face = "bold", size = 15),
+    plot.background = element_rect(fill = "#636363"),
+    panel.background = element_rect(fill = "#484848"),
+    panel.grid = element_line(color = "#636363"),
+    axis.text = element_text(color = "#EAEAEA"),
+    panel.grid.minor = element_blank()
   )
-
-#create the animation
-points_anim <- points +
-  transition_reveal(Game)
 
 # display plot
 points
 
+# create the animation
+#points_anim <- points +
+ #transition_reveal(Game)
+
 # display animation
-# points_anim
+#points_anim
 
 # save the animation to a .mp4 file
-anim_save(file = "lebron.mp4", animation = points_anim, path = "/Users/mgillis/Desktop/projects/nba-scoring")
+#anim_save(file = "lebron.mp4", animation = points_anim, path = "/Users/mgillis/Desktop/projects/nba-scoring")
