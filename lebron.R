f2e81fbe83f8d695320122a66d8d401d68497292
mgillis33 <gillism@emmanuel.edu>
2023-02-05 23:42:53 -0500

    Added team as a group in aes.

--- lebron.R
+++ lebron.R
@@ -6,11 +6,11 @@ library(gganimate)
 lebron <- read.csv("/Users/mgillis/Desktop/Projects/nba-scoring/data/Lebron.csv")
 
 # build plot
-points <- ggplot(lebron, aes(x = Game, y = CUMPTS)) +
+points <- ggplot(lebron, aes(x = Game, y = CUMPTS, group = Tm)) +
   geom_line(linewidth = 1, color = "#EAEAEA") +
   geom_hline(yintercept = 38387, linewidth = 1, color = "#EAEAEA") +
   geom_point(size = 2, color = "#EAEAEA") + 
-  annotate("text", x = 1180, y = 3400, label = "test", family = "Rockwell", size = 15, color = "#EAEAEA") +
+  annotate("text", x = 1150, y = 2500, label = lebron$Date[30], family = "Rockwell", size = 10, color = "#EAEAEA") +
   labs(
     title = "Lebron James: Journey to Career Points Record",
     subtitle = "An animation of Lebron's total career points, as he breaks Kareem Abdul-Jabbar's record for most career points.",
@@ -24,7 +24,7 @@ points <- ggplot(lebron, aes(x = Game, y = CUMPTS)) +
     axis.title = element_text(face = "bold", size = 15),
     plot.background = element_rect(fill = "#636363"),
     panel.background = element_rect(fill = "#484848"),
-    panel.grid = element_line(color = "#636363"),
+    panel.grid = element_blank(),
     axis.text = element_text(color = "#EAEAEA"),
     panel.grid.minor = element_blank()
   )
@@ -33,11 +33,11 @@ points <- ggplot(lebron, aes(x = Game, y = CUMPTS)) +
 points
 
 # create the animation
-#points_anim <- points +
- #transition_reveal(Game)
+points_anim <- points +
+ transition_reveal(Game)
 
 # display animation
-#points_anim
+points_anim
 
 # save the animation to a .mp4 file
-#anim_save(file = "lebron.mp4", animation = points_anim, path = "/Users/mgillis/Desktop/projects/nba-scoring")
+anim_save(file = "lebron.gif", animation = points_anim, path = "/Users/mgillis/Desktop/projects/nba-scoring")
