955aab0df66ddd28573e379264f57ac45ddf181b
mgillis33 <gillism@emmanuel.edu>
2023-02-08 19:42:55 -0500

    Made some theme changes. Played around with Cowplot. Added images folder for annotations.

--- lebron.R
+++ lebron.R
@@ -1,9 +1,15 @@
 # load packages
 library(tidyverse)
 library(gganimate)
+library(scales)
+library(cowplot)
+library(magick)
 
 # load data
 lebron <- read.csv("/Users/mgillis/Desktop/Projects/nba-scoring/data/Lebron.csv")
+
+# load images
+crown <- image_read("/Users/mgillis/Desktop/Projects/nba-scoring/images/Crown_of_England_Old.png")
 
 # build plot
 points <- ggplot(lebron, aes(x = Game, y = CUMPTS, group = Tm)) +
@@ -11,33 +17,39 @@ points <- ggplot(lebron, aes(x = Game, y = CUMPTS, group = Tm)) +
   geom_hline(yintercept = 38387, linewidth = 1, color = "#EAEAEA") +
   geom_point(size = 2, color = "#EAEAEA") + 
   annotate("text", x = 1150, y = 2500, label = lebron$Date[30], family = "Rockwell", size = 10, color = "#EAEAEA") +
+  scale_y_continuous(labels = label_number(suffix = "K", scale = 1e-3)) +
   labs(
     title = "Lebron James: Journey to Career Points Record",
-    subtitle = "An animation of Lebron's total career points, as he breaks Kareem Abdul-Jabbar's record for most career points.",
+    subtitle = "An animation of Lebron's total career points, as he breaks Kareem Abdul-Jabbar's \n record for most career points.",
     x = "Games (Career)",
     y = "Points (Total)"
     ) +
   theme(
-    text = element_text(family = "Rockwell", color = "#EAEAEA"),
-    plot.title = element_text(face = "bold", size = 33),
-    plot.subtitle = element_text(size = 15),
-    axis.title = element_text(face = "bold", size = 15),
+    text = element_text(family = "Trebuchet MS", color = "#EAEAEA"),
+    plot.title = element_text(face = "bold", size = 24, hjust = 0.5),
+    plot.subtitle = element_text(size = 15, hjust = 0.5),
+    axis.title = element_text(face = "bold", size = 20),
     plot.background = element_rect(fill = "#636363"),
     panel.background = element_rect(fill = "#484848"),
     panel.grid = element_blank(),
-    axis.text = element_text(color = "#EAEAEA"),
+    axis.text = element_text(color = "#EAEAEA", size = 15),
+    axis.ticks = element_line(color = "#EAEAEA", size = 2),
     panel.grid.minor = element_blank()
   )
 
+# image annotations
+final <- ggdraw(points) +
+  draw_image(crown, scale = 0.05, x = -0.4, y = 0.48)
+
 # display plot
-points
+final
 
 # create the animation
-points_anim <- points +
- transition_reveal(Game)
+#points_anim <- points +
+ #transition_reveal(Game)
 
 # display animation
-points_anim
+#points_anim
 
 # save the animation to a .mp4 file
-anim_save(file = "lebron.gif", animation = points_anim, path = "/Users/mgillis/Desktop/projects/nba-scoring")
+#anim_save(file = "lebron.gif", animation = points_anim, path = "/Users/mgillis/Desktop/projects/nba-scoring")
